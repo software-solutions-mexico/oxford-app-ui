6fa62bbfb9c371dbef7fcea3225e41e1da96a2fb
Oscar Michel <oscarmichelh@gmail.com>
2019-10-17 11:37:25 -0500

    Added login logic

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ angular.module("main").controller("mainController", function ($scope, $http) {
     $http.defaults.headers.post["Content-Type"] = "application/json";
 
     $scope.username = "oscar@mail.com";
-    $scope.password = "123456";
+    $scope.password = "";
+    $scope.user_email = sessionStorage.getItem("user_email");
+    $scope.token = sessionStorage.getItem("token");
     $scope.main = 'views/home.html';
 
     $scope.login = function (user, password) {
@@ -16,11 +18,23 @@ angular.module("main").controller("mainController", function ($scope, $http) {
         })
             .then(function (response) {
                 $scope.token = response.data.data.token;
+                sessionStorage.setItem("token", $scope.token);
+
                 $scope.user = response.data.data.user;
+                $scope.user_email = $scope.user.email;
+                sessionStorage.setItem("user_email", $scope.user.email);
             }, function (response) {
                 $scope.login_error = response.data.errors;
             });
         return;
     };
+
+    $scope.logout = function () {
+        $scope.token = "";
+        sessionStorage.setItem("token", "");
+        console.log("FROM LOGoUT");
+    };
+
+    // $scope.login($scope.username, $scope.password);
 
 });
